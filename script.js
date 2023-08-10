c83e522c4f9f9af7ce4b163bc34fe7352d81bf81
Krishan Chakroborty <138010864+krisHChakroborty@users.noreply.github.com>
2023-08-10 23:13:53 +0530

    NEW

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
-    $(".change").click(function() {
+    $(".white").click(function() {
         $("#changeBackground").css("background-color", "antiquewhite");
         $("#changeBackground").css("background", "none");
         $(".text_black").css("color", "black");
+    });
+});
+
+
+$(document).ready(function() {
+    $(".skyblue").click(function() {
+        $("#changeBackground").css("background", "rgb(182, 221, 255)");
+        // $("#changeBackground").css("background", "none");
+        $(".text_black").css("color", "black");
+    });
+});
+
+
+$(document).ready(function() {
+    $(".blue").click(function() {
+        $("#changeBackground").css("background", "#0013f5");
+        $("#changeBackground").css("border", "black");
+        $(".text_black").css("color", "white");
+    });
+});
+
+$(document).ready(function() {
+    $(".black").click(function() {
+        $("#changeBackground").css("background", "black");
+        // $("#changeBackground").css("background", "none");
+        $(".text_black").css("color", "white");
     });
 });
 
