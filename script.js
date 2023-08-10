bac079f33fb0ace599b31c436fe4ff10dc511866
Krishan Chakroborty <138010864+krisHChakroborty@users.noreply.github.com>
2023-08-10 21:17:42 +0530

    new

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".change").click(function() {
+        $("#changeBackground").css("background-color", "antiquewhite");
+        $("#changeBackground").css("background", "none");
+        $(".text_black").css("color", "black");
+    });
+});
+
